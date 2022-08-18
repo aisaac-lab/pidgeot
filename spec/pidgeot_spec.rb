59d13a39312c84959e84b37068e5e2812a861703
gogotanaka <mail@tanakakazuki.com>
2022-08-19 00:08:02 +0900

    init

---
+++ spec/pidgeot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pidgeot do
+  it "has a version number" do
+    expect(Pidgeot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
